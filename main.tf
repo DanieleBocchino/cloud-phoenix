e6c7713d7f3ad6f4e24189c339fda5993d3165c0
DaneieleBocchino <bocchino.daniele@gmail.com>
2023-10-24 20:27:41 +0200

    add provider

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+
+
+
